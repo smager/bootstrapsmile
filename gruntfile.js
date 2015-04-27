06a0d094a7d2ceb8d557785523446a013afe80c2
German M. Fuentes <gm_fuentes@yahoo.com>
2015-04-27 15:59:20 -0400

    update gruntfile.js for auto install bower dependencies

--- gruntfile.js
+++ gruntfile.js
@@ -49,6 +49,23 @@ module.exports = function (grunt) {
        , buildthemes: themes.list  
     });
 
+    
+    grunt.registerTask('copybower', 'copy bower.json', function() {
+        grunt.log.writeln('install bower.json...');
+            var copyfiles = [
+                {  //copy client side package
+                      expand: true
+                    , src: ['bower.json']
+                    , dest: 'dist/themes/'
+                    , cwd: 'src/'
+                    , filter: 'isFile'
+
+                }            
+            ];
+            grunt.config('copy.main.files',copyfiles);
+            grunt.task.run('copy');                                
+    });
+    
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
@@ -61,8 +78,26 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     
     
-    grunt.registerTask('default', ['buildthemes','express', 'open', 'watch']);
+    grunt.registerTask('build_and_run', ['buildthemes','express', 'open', 'watch']);
     //grunt.registerTask('default', ['buildthemes']);
+   
+    grunt.registerTask('default', 'install bower dependencies', function(){        
+        var tasks=[];        
+        var is_bower_dir_exist = grunt.file.isDir('./dist/themes/bower_components/');                
+        if(!is_bower_dir_exist) tasks.push('install_bower')                
+        tasks.push('build_and_run');        
+        grunt.task.run( tasks);         
+    });
+    
+
+    grunt.registerTask('run_grunt', 're execute grunt', function(){        
+        var exec = require('child_process').exec;
+        var cb = this.async();        
+        exec('grunt', function() {cb();});
+        grunt.log.writeln('Please wait to open the browser.');     
+
+    });    
+            
     
     grunt.registerTask('build', function(theme) {
         grunt.log.writeln( ['building... (' + theme + ')' ] );
@@ -91,15 +126,7 @@ module.exports = function (grunt) {
                 , dest: 'dist/themes/'+ theme +'/fonts/'
                 , cwd: 'node_modules/bootstrap/dist/fonts/'
                 , filter: 'isFile'
-            }                
-            ,{  //copy client side package
-                  expand: true
-                , src: ['bower.json']
-                , dest: 'dist/themes/'
-                , cwd: 'src/' + theme +'/'
-                , filter: 'isFile'
-                
-            }            
+            }                        
             ,{ //copy client side package
                   expand: true
                 , src: ['index.html']
@@ -124,5 +151,27 @@ module.exports = function (grunt) {
     grunt.registerMultiTask('buildthemes', function() {
         grunt.task.run('build:' + this.target);
     });
+    
+    
+
+    // grunt install [npm and bower ]
+    grunt.registerTask('install_bower_components', 'install bower', function() {
+        var exec = require('child_process').exec;
+        var cb = this.async();
+            grunt.log.writeln('Installing brower libraries...');                    
+            exec('bower install', {cwd: './dist/themes/'}, function(err, stdout, stderr) {
+                console.log(stdout);
+                cb();
+            });            
+    });
+    
+    
+  
+    //Task: install            
+    grunt.registerTask('install_bower', 'install node and bower dependencies', function(){ 
+        grunt.task.run(['copybower','install_bower_components']);         
+    });    
+    
+    
         
 };
