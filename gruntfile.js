d4246e94a4df8df3a6d5b27f0d90f98eb3329226
German M. Fuentes <gm_fuentes@yahoo.com>
2015-04-28 16:17:03 +0800

    transfer bootstrap from node_modules to bower_components path

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,6 @@
 //'use strict';
 var path = require('path').resolve('.');
+var bsPath = 'dist/themes/bower_components/bootstrap/dist/';
 module.exports = function (grunt) {
     var themes = grunt.file.readJSON('themes-config.json');
 
@@ -100,22 +101,23 @@ module.exports = function (grunt) {
     
     grunt.registerTask('build', function(theme) {
         grunt.log.writeln( ['building... (' + theme + ')' ] );
-        var bs= 'bootstrap';
-        var th = 'theme';
+        var ns = "bss.";
+        var bs=  'bootstrap';
+        var th =  'theme';
         var dist = 'dist/themes/' + theme + '/css/';
         var src = 'src/' + theme + '/';
         var css = ".css";
         var less = ".less";
-        var minfile = bs + '.min' + css;
+        var minfile = ns + bs + '.min' + css;
 
         var files ={};
-            files[dist + bs + css] = src + bs + less;
-            files[dist + th + css] = src + th + less; 
+            files[dist + ns + bs + css] = src + bs + less;
+            files[dist + ns + th + css] = src + th + less; 
 
         grunt.config('less.dev.files', files);
 
         var cssminfiles={};        
-        cssminfiles[dist + minfile] =  [dist + bs + css, dist + th + css];
+        cssminfiles[dist + minfile] =  [dist + ns + bs + css, dist + ns + th + css];
         grunt.config('cssmin.target.files', cssminfiles);
     
         var copyfiles = [
@@ -123,17 +125,26 @@ module.exports = function (grunt) {
                   expand: true
                 , src: ['**']
                 , dest: 'dist/themes/'+ theme +'/fonts/'
-                , cwd: 'node_modules/bootstrap/dist/fonts/'
+                , cwd: bsPath + 'fonts/'  
                 , filter: 'isFile'
             }                                   
             ,{ //copy bootstrap.js
                 expand: true
                 , src: ['bootstrap*.min.js']
                 , dest: 'dist/themes/'+ theme +'/js/'
-                , cwd: 'node_modules/bootstrap/dist/js/'
+                , cwd:  bsPath + 'js/'
                 , filter: 'isFile'
                 
             }
+            ,{ //copy .less
+                expand: true
+                , src: ['*.less']
+                , dest: 'dist/themes/'+ theme +'/less/'
+                , cwd: 'src/' + theme +'/'
+                , filter: 'isFile'
+                
+            }            
+            
         ];
         grunt.config('copy.main.files',copyfiles);
         grunt.task.run(['less:dev','cssmin','copy','create_template:' + theme]);
